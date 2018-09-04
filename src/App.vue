8e2764b017787a865fc46d45bd81d024f92b70ab
pangjie___ <1406704102@qq.com>
2018-09-04 10:47:03 +0800

    测试提交

--- src/App.vue
+++ src/App.vue
@@ -11,5 +11,5 @@ export default {
 </script>
 <style>
   @import "../static/css/main.css";
-  @import "../static/css/color-dark.css";     /*深色主题*/
+  @import "../static/css/color-dark.css"; /*深色主题11111*/
 </style>
